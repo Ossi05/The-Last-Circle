0725cf04217e0fe9dd29e4b7cd86c19ef1533d06
Ossi <ossijuhani.ikonen@gmail.com>
2024-06-06 12:38:48 +0300

    Bug fixed
    Fixed a bug where movement bindings reset key wasn't working.

--- Assets/Samples/Input System/1.8.2/Rebinding UI/RebindActionUI.cs
+++ Assets/Samples/Input System/1.8.2/Rebinding UI/RebindActionUI.cs
@@ -218,18 +218,18 @@ namespace UnityEngine.InputSystem.Samples.RebindUI
             if (!ResolveActionAndBinding(out var action, out var bindingIndex))
                 return;
 
-            ResetBinding(action, bindingIndex);
+            // ResetBinding(action, bindingIndex);
 
-            //if (action.bindings[bindingIndex].isComposite)
-            //{
-            //    // It's a composite. Remove overrides from part bindings.
-            //    for (var i = bindingIndex + 1; i < action.bindings.Count && action.bindings[i].isPartOfComposite; ++i)
-            //        action.RemoveBindingOverride(i);
-            //}
-            //else
-            //{
-            //    action.RemoveBindingOverride(bindingIndex);
-            //}
+            if (action.bindings[bindingIndex].isComposite)
+            {
+                // It's a composite. Remove overrides from part bindings.
+                for (var i = bindingIndex + 1; i < action.bindings.Count && action.bindings[i].isPartOfComposite; ++i)
+                    action.RemoveBindingOverride(i);
+            }
+            else
+            {
+                action.RemoveBindingOverride(bindingIndex);
+            }
 
             UpdateBindingDisplay();
         }
